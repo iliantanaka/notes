bef86382395eb31f7f35dadcd07fce0f53ce8c0a
Ilian Alves Tanaka de Carvalho <38259435+iliantanaka@users.noreply.github.com>
2025-12-18 17:03:09 -0300

    Controller para autenticação

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function login()
+    {
+        echo 'Login';
+    }
+    function logout()
+    {
+        echo 'Logout';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+
+// Rotas de autenticação
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
